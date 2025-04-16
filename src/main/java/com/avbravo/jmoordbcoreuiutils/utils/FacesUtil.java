debc78879b6a67b82724b5bc727dcfa9a4eb1216
avbravo <avbravo@avbravo-IdeaPad-Gaming-3-15ARH7>
2025-04-15 22:44:09 -0500

    0.2

--- src/main/java/com/avbravo/jmoordbcoreuiutils/utils/FacesUtil.java
+++ src/main/java/com/avbravo/jmoordbcoreuiutils/utils/FacesUtil.java
@@ -8,43 +8,44 @@ import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
+import java.util.UUID;
 
 /**
  *
  * @author avbravo
  */
-@Named
-@RequestScoped
+//@Named
+//@RequestScoped
 public class FacesUtil {
- private static String opertativeSystem = System.getProperty("os.name").toLowerCase();
+
+    private static String opertativeSystem = System.getProperty("os.name").toLowerCase();
+
     /**
      * Creates a new instance of JakartaFacesUtil
      */
- 
 
-    public void addMessage(FacesMessage.Severity severity, String summary, String detail) {
+    public static void addMessage(FacesMessage.Severity severity, String summary, String detail) {
         FacesContext.getCurrentInstance().
                 addMessage(null, new FacesMessage(severity, summary, detail));
     }
 
-    public void showInfo(String msg) {
+    public static void showInfo(String msg) {
         addMessage(FacesMessage.SEVERITY_INFO, "Info Message", msg);
     }
 
-    public void showWarn(String msg) {
+    public static void showWarn(String msg) {
         addMessage(FacesMessage.SEVERITY_WARN, "Warn Message", msg);
     }
 
-    public void showError(String msg) {
+    public static void showError(String msg) {
         addMessage(FacesMessage.SEVERITY_ERROR, "Error Message", msg);
     }
 
-    public void showSticky(String msg) {
+    public static void showSticky(String msg) {
         FacesContext.getCurrentInstance().addMessage("sticky-key", new FacesMessage(FacesMessage.SEVERITY_INFO, "Sticky Message", msg));
     }
 
-    
-       // <editor-fold defaultstate="collapsed" desc="nameOfClassAndMethod()">
+    // <editor-fold defaultstate="collapsed" desc="nameOfClassAndMethod()">
     public static String nameOfClassAndMethod() {
         final StackTraceElement e = Thread.currentThread().getStackTrace()[2];
         final String s = e.getClassName();
@@ -65,9 +66,8 @@ public class FacesUtil {
         return e.getMethodName();
     }
     // </editor-fold>
-    
-    
-       // <editor-fold defaultstate="collapsed" desc="isWindows()">
+
+    // <editor-fold defaultstate="collapsed" desc="isWindows()">
     /*
     Implementado desde el ejemplo de Mkyong
     https://mkyong.com/java/how-to-detect-os-in-java-systemgetpropertyosname/
@@ -95,17 +95,87 @@ public class FacesUtil {
     }
 // </editor-fold>
 // <editor-fold defaultstate="collapsed" desc="boolean isSolaris()">
+
     public static boolean isSolaris() {
 
         return (opertativeSystem.indexOf("sunos") >= 0);
 
     }
     // </editor-fold>
-    
-      public static String userHome() {
+    // <editor-fold defaultstate="collapsed" desc="String userHome()">
+    public static String userHome() {
         return System.getProperty("user.home");
 
     }
-      
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="isVacio(String texto)()"> 
+    /**
+     * return true si es null empty equals("")
+     *
+     * @param texto
+     * @return
+     */
+    public static Boolean isVacio(String texto) {
+        texto = texto.trim();
+        return texto == null || texto.equals("") || texto.isEmpty();
+    }// </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="isVacio(Integer texto)"> 
+
+    /**
+     * return true si es null empty equals("")
+     *
+     * @param texto
+     * @return
+     */
+    public static Boolean isVacio(Integer texto) {
+        return texto == null || texto.equals("");
+    }// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="isVacio(Integer texto)"> 
+    /**
+     * return true si es null empty equals("")
+     *
+     * @param texto
+     * @return
+     */
+    public static Boolean isVacio(Double texto) {
+        return texto == null || texto.equals("");
+    }// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Boolean isNegativo(Double texto)"> 
+    /**
+     * return true si es null empty equals("")
+     *
+     * @param texto
+     * @return
+     */
+    public static Boolean isNegativo(Double numero) {
+        return numero == null || numero.equals("") || numero < 0;
+    }// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Boolean isNegativo(Double texto)"> 
+    /**
+     * return true si es null empty equals("")
+     *
+     * @param texto
+     * @return
+     */
+    public static Boolean isNegativo(Integer numero) {
+        return numero == null || numero.equals("") || numero < 0;
+    }// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="getUUID"> 
+    /**
+     * genera id
+     *
+     * @return returna un randomUUID automatico
+     */
+    public static String getUUID() {
+
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+
+    }// </editor-fold>
 
 }
